0bcd970e2e4bd7e17bf07b7dc59ebf048b7a8c1e
Hankrezu <hankrezu@gmail.com>
2024-12-09 12:37:41 +0700

    redo warehouse

--- src/pages/admin/warehouses/detail/index.jsx
+++ src/pages/admin/warehouses/detail/index.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Text, HStack, Flex, Divider, Box } from "@chakra-ui/react";
+import { LuChevronRight } from "react-icons/lu";
+import { useParams } from "react-router-dom";
+import WarehouseTable from "../../components/warehouses/WarehouseTable";
+
+import { DataContext } from "../../../../context/Context";
+import Zones from "../../components/warehouses/Zones";
 
 const WareshousesDetail = () => {
-  return (
-    <div>Day la trang Detail WaresHouse</div>
-  )
-}
+  const { zones } = useContext(DataContext);
+  const [rackId, setRackId] = useState(null)
+  const { warehouseId } = useParams();
 
-export default WareshousesDetail
+  console.log("rackId : ",rackId)
+  return (
+    <Box>
+      <Flex justifyContent={"space-between"} p={6}>
+        <Text fontSize="xl" fontWeight="medium">
+          Warehouses
+        </Text>
+
+        <HStack>
+          <Text
+            fontSize="l"
+            fontWeight="medium"
+            _hover={{
+              color: "orange.500",
+              cursor: "pointer",
+              transition: "all, 0.5s",
+            }}
+          >
+            Warehouses
+          </Text>
+          <LuChevronRight />
+          <Text fontSize="l" fontWeight="medium" color={"orange.400"}>
+            List Warehouses
+          </Text>
+        </HStack>
+      </Flex>
+      <Flex>
+        <Flex width={"60%"} gap={4} alignItems={'center'} justifyContent={'center'}>
+          {zones
+            .filter((item) => item.warehouse == warehouseId)
+            .map((zone) => (
+              <Zones zone={zone} setRackId={setRackId}/>
+            ))}
+        </Flex>
+        <Flex width={"40%"} background={"yellow.100"}>{rackId}</Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default WareshousesDetail;
